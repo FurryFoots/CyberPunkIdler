ce34702b47909f704a56a3fa14572d3a2933cd09
FurryFoots <166951094+FurryFoots@users.noreply.github.com>
2026-06-26 14:27:08 -0400

    Gun Unlock and Gun Purchase

---
+++ Scenes/game_state.gd
@@ -0,0 +1,32 @@
+extends Node
+@onready var player = $"../Player"
+
+#Purchase Gun -> Mug Civilian
+@onready var purchase_gun_button: Button = $"../PlayArea/MugCivilian/PurchaseGun"
+@onready var mug_civilian_button: Button = $"../PlayArea/MugCivilian/MugCivilianButton"
+@onready var mug_civilian_progress_bar: ProgressBar = $"../PlayArea/MugCivilian/MugCivilianProgressBar"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func purchase_gun_available() -> void:
+	purchase_gun_button.visible = true
+	
+func purchase_gun_purchased() -> void:
+	purchase_gun_button.visible = false
+	mug_civilian_button.visible = true
+	mug_civilian_progress_bar.visible = true
+
+#PurchaseGun Button Pressed
+func _on_purchase_gun_pressed() -> void:
+	if player.cash >= 10:
+		purchase_gun_purchased()
+		player.modify_cash(-10)
+		player.gun_purchased = true
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,10 +3,16 @@ extends Node
 @onready var cash_rich_text_label_number: RichTextLabel = $"../PlayArea/PlayerStats/CashRichTextLabelNumber"
 @onready var cred_rich_text_label_number: RichTextLabel = $"../PlayArea/PlayerStats/CredRichTextLabelNumber"
 @onready var heat_rich_text_label_number: RichTextLabel = $"../PlayArea/PlayerStats/HeatRichTextLabelNumber"
+@onready var game_state: Node = $"../GameState"
 
-var Cash:float = 0
-var Cred:float = 0
-var Heat:float = 0
+@export var cash:float = 0
+@export var cred:float = 0
+@export var heat:float = 0
+
+var gun_unlocked: bool = false
+var gun_purchased: bool = false
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,22 +21,33 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	check_for_unlocks()
 
 func modify_cash(cash_change: float) -> void:
-	Cash += cash_change
-	if Cash < 0:
-		Cash = 0
-	cash_rich_text_label_number.text = str(Cash)
+	cash += cash_change
+	if cash < 0:
+		cash = 0
+	cash_rich_text_label_number.text = str(cash)
 	
 func modify_cred(cred_change: float) -> void:
-	Cred += cred_change
-	if Cred < 0:
-		Cred = 0
-	cred_rich_text_label_number.text = str(Cred)
+	cred += cred_change
+	if cred < 0:
+		cred = 0
+	cred_rich_text_label_number.text = str(cred)
 	
 func modify_heat(heat_change: float) -> void:
-	Heat += heat_change
-	if Heat < 0:
-		Heat = 0
-	heat_rich_text_label_number.text = str(Heat)
+	heat += heat_change
+	if heat < 0:
+		heat = 0
+	heat_rich_text_label_number.text = str(heat)
+	
+# Place where new unlocks will be added:
+# gun_unlocked -> First time greater than 10 Cash -> purchase_gun_available()
+
+func check_for_unlocks() -> void:
+	if(!gun_unlocked):
+		if cash >= 10:
+			game_state.purchase_gun_available()
+			gun_unlocked = true
+			
+	
