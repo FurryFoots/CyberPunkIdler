adb522c7963b2c2bd1c674ec9e2fd5ea9453f0f0
FurryFoots <166951094+FurryFoots@users.noreply.github.com>
2026-06-26 13:37:46 -0400

    Fixed Start Button

--- Scripts/start_button.gd
+++ Scripts/start_button.gd
@@ -12,5 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://CyberPunkIdler/Scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 	pass # Replace with function body.
